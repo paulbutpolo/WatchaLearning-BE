b42eda2de42a7c6bf836a1e2182b5b8fa6ecc947
paulbutpolo <tmq.pauljohnsopranes@gmail.com>
2025-02-27 00:35:58 +0800

    Additional routes

---
+++ routes/subscriberRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const subscriberController = require('../controllers/subscriberController')
+const authMiddleware = require('../middleware/auth');
+
+router.post('/subscriber/create', authMiddleware, subscriberController.createSubscriber)
+router.post('/subscriber/delete', authMiddleware, subscriberController.unsubscribe);
+router.get('/subscriber/check', authMiddleware, subscriberController.checkSubscription);
+router.put('/subscriber/update-progress', authMiddleware, subscriberController.updateProgress);
+
+module.exports = router;
